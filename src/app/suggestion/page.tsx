ea1a9629bc08cfaaf90ba5c17978a9a6957b9115
Muhammad Asadullah <masdullah230@gmail.com>
2025-01-27 21:52:45 +0500

    update

--- src/app/suggestion/page.tsx
+++ src/app/suggestion/page.tsx
@@ -72,7 +72,7 @@ WHERE
     return (
       <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto">
-        <h1 className="text-2xl md:text-4xl font-bold text-center text-white mb-8 uppercase">SGGESTED CROP {cropData[0].name}</h1>
+        <h1 className="text-2xl md:text-4xl font-bold text-center text-white mb-8 uppercase">SUGGESTED CROP {cropData[0].name}</h1>
 
         <div className="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
           <Image
